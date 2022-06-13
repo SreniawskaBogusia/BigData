792de12a5a812ccfef9dc129e5f9cdb043b94b84
Bogusława Śreniawska <65511124+SreniawskaBogusia@users.noreply.github.com>
2022-06-13 13:58:35 +0200

    adding lab 5

---
+++ lab5/Ćwiczenia 5.scala
@@ -0,0 +1,22 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC ## Azure 
+// MAGIC - Data Lake Analytics -  jest usługą zadań analizy na żądanie, która pozwala uprościć analizowanie danych big data <https://docs.microsoft.com/en-us/azure/data-lake-analytics/> 
+// MAGIC - Synapse Analytics - nieograniczona usługa analizy, która łączy integrację danych, magazynowanie danych przedsiębiorstwa z analizą danych big data <https://docs.microsoft.com/en-us/azure/synapse-analytics/>
+// MAGIC - Data Factory - prosta integracja danych hybrydowych na skalę przedsiębiorstwa <https://docs.microsoft.com/en-us/azure/data-factory/>
+// MAGIC - Databricks - projektowanie rozwiązań AI przy użyciu analiz opartych na platformie Apache Spark™ <https://docs.microsoft.com/en-us/azure/databricks/>
+// MAGIC - Machine Learning - usługa klasy korporacyjnej dla kompleksowego cyklu życia uczenia maszynowego <https://docs.microsoft.com/en-us/azure/machine-learning/>
+// MAGIC - Data Share - prosta i bezpieczna usługa do udostępniania danych big data <https://docs.microsoft.com/en-us/azure/data-share/>
+// MAGIC - Purview - ujednolicone rozwiązanie do zapewniania ładu danych, które ułatwia zarządzanie danymi lokalnymi, wielochmurowymi i SaaS <https://docs.microsoft.com/en-us/azure/purview/>
+// MAGIC 
+// MAGIC ## AWS
+// MAGIC - Athena -  interaktywna usługa której zadaniem jest ułatwianie analizy danych w Amazon S3 przy użyciu standardowego SQL <https://docs.aws.amazon.com/athena/>
+// MAGIC - Data Pipeline - usługa sieciowa, której można używać do automatyzacji przenoszenia i przekształcania danych <https://docs.aws.amazon.com/data-pipeline/>
+// MAGIC - Lake Formation - scentralizowane, nadzorowane i zabezpieczone repozytorium, w którym przechowywane są wszystkie Twoje dane, zarówno w oryginalnej formie, jak i przygotowane do analizy <https://docs.aws.amazon.com/lake-formation/>
+// MAGIC - Glue - rozwiązaniem do katalogowania i przetwarzania danych (za pomocą technologii Apache Hive i Apache Spark) <https://docs.aws.amazon.com/glue/>
+// MAGIC - OpenSearch Services - usługa zarządzana, która ułatwia wdrażanie, obsługę i skalowanie klastrów OpenSearch w chmurze AWS <https://docs.aws.amazon.com/opensearch-service/>
+// MAGIC - CloudSearch - usługa w chmurze, która ułatwia konfigurację, zarządzanie i skalowanie rozwiązania wyszukiwania dla Twojej witryny lub aplikacji <https://docs.aws.amazon.com/cloudsearch/>
+// MAGIC - EMR - narzędzie umożliwiające szybkie i efektywne kosztowo przetwarzanie ogromnych ilości danych <https://docs.aws.amazon.com/emr/>
+
+// COMMAND ----------
+
