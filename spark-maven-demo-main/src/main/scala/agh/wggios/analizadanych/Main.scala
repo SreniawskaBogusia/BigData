716f1a30c61bfba4f1bcb54fcc04e334378017ea
Bogusława Śreniawska <65511124+SreniawskaBogusia@users.noreply.github.com>
2022-06-21 09:25:02 +0200

    adding lab 13 - logs

--- spark-maven-demo-main/src/main/scala/agh/wggios/analizadanych/Main.scala
+++ spark-maven-demo-main/src/main/scala/agh/wggios/analizadanych/Main.scala
@@ -5,6 +5,7 @@ import agh.wggios.analizadanych.caseclass.Flights
 import agh.wggios.analizadanych.datareader.DataReader
 import agh.wggios.analizadanych.transformations.Transformation
 import org.apache.spark.sql.SparkSession
+import org.apache.log4j.Logger
 
 object Main {
 
@@ -15,6 +16,8 @@ object Main {
       .getOrCreate();
 
     import spark.implicits._
+    lazy val logger:Logger=Logger.getLogger(getClass.getName)
+    logger.info("LOG Info")
     val df = new DataReader().readData(spark,"2013-summary.csv").as[Flights]
     df.filter(row => new Transformation(150).airport_filtering(row)).show()
 
